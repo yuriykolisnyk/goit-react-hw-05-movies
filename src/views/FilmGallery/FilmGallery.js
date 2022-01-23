3f8129356d860cc5eb5cde23a524e022fdac26db
yuriykolisnyk <ykolisnyk@gmail.com>
2022-01-23 11:29:07 +0200

    fixed

--- src/views/FilmGallery/FilmGallery.js
+++ src/views/FilmGallery/FilmGallery.js
@@ -1,10 +1,11 @@
-import { Link, useRouteMatch } from 'react-router-dom';
+import { Link, useRouteMatch, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { POSTER_URL } from '../../API/MoviesAPI';
 import s from './FilmGallery.module.css';
 
 function FilmGallery({ films }) {
   const { url } = useRouteMatch();
+  const location = useLocation();
 
   return (
     <ul className={s.list}>
@@ -12,7 +13,10 @@ function FilmGallery({ films }) {
         <>
           {film.poster_path && (
             <li key={film.id} className={s.item}>
-              <Link to={`${url}/${film.id}`} className={s.link}>
+              <Link
+                to={{ pathname: `${url}/${film.id}`, state: { from: location } }}
+                className={s.link}
+              >
                 <img
                   className={s.image}
                   src={POSTER_URL + film.poster_path}
